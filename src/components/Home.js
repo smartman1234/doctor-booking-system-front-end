d8d9e77a9eb0c147a2045831848d0b017fd1e30a
smartman1234 <john.smith.dev0724@gmail.com>
2021-06-22 02:27:57 +0200

    get all doctor data from backend

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { useAlert } from "react-alert";
 import Pagination from "react-js-pagination";
 function Home(props) {
   const [data, setData] = useState([]);
+  //const [profileImg,setProfileImg] = useState();
   const [current_page,setCurrent_page] = useState(0);
  const [per_page,setPer_page] = useState(0);
  const [total,setTotal] = useState(0);
@@ -20,7 +21,7 @@ function Home(props) {
     })
       .then((response) => response.json())
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
         setData(res.data);
         setCurrent_page(res.current_page);
         setPer_page(res.per_page);
@@ -46,6 +47,8 @@ function Home(props) {
 
 }
 
+    
+
   const alert = useAlert();
   const notify = () => {
     // SuccessNotify("hello word");
@@ -57,7 +60,24 @@ function Home(props) {
                 <div className="card mb-4" key={doctor.id}>
                     <div className="card-header">{doctor.name_en}</div>
                     <div className="card-header">{doctor.name_ar}</div>
+                    <div className="card-header">{doctor.gender}</div>
+                    <div className="card-header">{doctor.country.name_en}</div>
+                    <div className="card-header">{doctor.degree.name_en}</div>
+                    <div className="card-header">{doctor.specialist.name_en}</div>
+                    <div className="card-header">{doctor.addresses.map(address => {
+                      return (
+                        <ol key={address.id}>
+                        <li>{address.address_en}</li>
+                        <li>{address.district.name_en}</li>
+                        <li>{address.district.city.name_en}</li>
+                        </ol>
+                        ); 
+                    })}</div>
+                    <div className="card-header">{doctor.subspecialists.map(subspecialist => {
+                      return (<li key={subspecialist.id}>{subspecialist.name_en}</li>); 
+                    })}</div>
                     <div className="card-body">
+                    <img style={{width:100}} src={"http://127.0.0.1:8000/storage/" + doctor.image}/>
                     <h5 className="card-title">{doctor.mobile}</h5>
                         <p className="card-text">
                             {doctor.session_time}
