0e5805b6bf7b7c8e79553316c9b31260c63826ac
smartman1234 <john.smith.dev0724@gmail.com>
2021-06-25 23:46:48 +0200

    Handel local storge

--- src/App.js
+++ src/App.js
@@ -33,6 +33,8 @@ function App() {
         // setDoctorData(index);
         setUser(index);
         setSearchParams(searchParams);
+        localStorage.setItem('data', JSON.stringify(index));
+    localStorage.setItem('searchParams', searchParams['specialty']);
       };
   //start
   const sendDataToParent = (index) => { // the callback. Use a better name
